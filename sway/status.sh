69f08e248b25da331857f60291bd385fc1bd4b9f
h8d13 <hadrien.loge@gmail.com>
2025-12-03 11:54:29 +0100

    add editor

--- sway/status.sh
+++ sway/status.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo "$(date +'%Y-%m-%d %H:%M') | RAM: $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
+echo "$(date +'%Y-%m-%d %H:%M') | RAM: $(free -h | awk '/Mem:/ {print $3 "/" $2}') | CPU: $(awk '/^cpu / {printf("%.1f%%", ($2+$4)*100/($2+$4+$5))}' /proc/stat)"
