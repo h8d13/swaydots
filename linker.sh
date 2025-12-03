97ea414e8accced3179c0ece0a405d1a2197ac72
hadean <hadean@alpmfs.lan>
2025-12-03 13:35:26 +0100

    typos

--- linker.sh
+++ linker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+doas rm -f /etc/environment
+rm -f ~/.config/sway/config
+
 doas ln -s ~/swaydots/etc/environment /etc/environment
 ln -s ~/swaydots/sway/config ~/.config/sway/config
 ln -s ~/swaydots/foot/foot.ini ~/.config/foot/foot.ini
