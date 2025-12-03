34c9228684be1a34fd1acf66a4856e06b347e830
h8d13 <hadrien.loge@gmail.com>
2025-12-03 12:12:20 +0100

    linker

---
+++ linker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+doas ln -s ~/swaydots/etc/environment /etc/environment
+ln -s ~/swaydots/sway/config ~/.config/sway/config
+ln -s ~/swaydots/foot/foot.ini ~/.config/foot/foot.ini
+ln -s ~/swaydots/.profile ~/.profile
+ln -s ~/swaydots/.ashrc ~/.ashrc
