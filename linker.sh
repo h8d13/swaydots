2b4c468a35c053bc089de3a984e5db54850ef6f7
h8d13 <hadrien.loge@gmail.com>
2025-12-03 15:31:47 +0100

    remove . pre

--- linker.sh
+++ linker.sh
@@ -12,8 +12,8 @@ ln -s ~/swaydots/sway/config ~/.config/sway/config
 ln -s ~/swaydots/foot/foot.ini ~/.config/foot/foot.ini
 
 ln -s ~/swaydots/aliases ~/config/aliases
-ln -s ~/swaydots/.profile ~/.profile
-ln -s ~/swaydots/.ashrc ~/.ashrc
+ln -s ~/swaydots/profile ~/.profile
+ln -s ~/swaydots/ashrc ~/.ashrc
 ln -s ~/swaydots/bashrc ~/.config/bash/bashrc
 ln -s ~/swaydots/zshrc ~/.config/zsh/zshrc
 
