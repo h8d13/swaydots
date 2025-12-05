9763f03a92363770cf044e625d8e07e97174a856
h8d13 <hadrien.loge@gmail.com>
2025-12-05 12:20:23 +0100

    add interface filter

--- sway/status.sh
+++ sway/status.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-echo "USR: $USER | DT: $(date +'%Y-%m-%d-%H:%M') | PSC: $(ps aux | wc -l) | RAM: $(free -h | awk '/Mem:/ {print $3 "/" $2}') | CPU: $(awk '/^cpu / {printf("%.1f%%", ($2+$4)*100/($2+$4+$5))}' /proc/stat) | NET: ↑$(cat /sys/class/net/eth0/statistics/tx_bytes | numfmt --to=iec) ↓$(cat /sys/class/net/eth0/statistics/rx_bytes | numfmt --to=iec) "
+interface=$(ip -o -4 route show to default | awk '{print $5}')
+
+echo "USR: $USER | DT: $(date +'%Y-%m-%d-%H:%M') | PSC: $(ps aux | wc -l) | RAM: $(free -h | awk '/Mem:/ {print $3 "/" $2}') | CPU: $(awk '/^cpu / {printf("%.1f%%", ($2+$4)*100/($2+$4+$5))}' /proc/stat) | NET: ↑$(cat /sys/class/net/$interface/statistics/tx_bytes | numfmt --to=iec) ↓$(cat /sys/class/net/$interface/statistics/rx_bytes | numfmt --to=iec) "
