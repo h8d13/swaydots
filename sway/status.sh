ca4dabe3d741782a48f3d21c9447fb39a9c8843d
h8d13 <hadrien.loge@gmail.com>
2025-12-05 12:30:28 +0100

    better status

--- sway/status.sh
+++ sway/status.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-interface=$(ip -o -4 route show to default | awk '{print $5}')
 
-echo "USR: $USER | DT: $(date +'%Y-%m-%d-%H:%M') | PSC: $(ps aux | wc -l) | RAM: $(free -h | awk '/Mem:/ {print $3 "/" $2}') | CPU: $(awk '/^cpu / {printf("%.1f%%", ($2+$4)*100/($2+$4+$5))}' /proc/stat) | NET: ↑$(cat /sys/class/net/$interface/statistics/tx_bytes | numfmt --to=iec) ↓$(cat /sys/class/net/$interface/statistics/rx_bytes | numfmt --to=iec) "
+interface=$(ip -o -4 route show to default | awk '{print $5}')
+procs=$(ls /proc | grep -E '^[0-9]+$' | wc -l)
+dt=$(date +'%Y-%m-%d-%H:%M')
+ram_use=$(free -h | awk '/Mem:/ {print $3 "/" $2}')
+cpu_use=$(awk '/^cpu / {printf("%.1f%%", ($2+$4)*100/($2+$4+$5))}' /proc/stat)
+tx=$(numfmt --to=iec < /sys/class/net/$interface/statistics/tx_bytes)
+rx=$(numfmt --to=iec < /sys/class/net/$interface/statistics/rx_bytes)
+
+echo "USR: $USER | DT: $dt | PSC: $procs | RAM: $ram_use | CPU: $cpu_use | NET: ↑$tx ↓$rx "
