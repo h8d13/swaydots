dcddd2004abc65eb30ad763b11013f6367aa2d4e
h8d13 <hadrien.loge@gmail.com>
2025-12-07 17:54:26 +0100

    typo and add micro settings

--- linker.sh
+++ linker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 mkdir -p ~/.config/bash
 mkdir -p ~/.config/zsh
 mkdir -p ~/.config/sway
@@ -13,11 +12,11 @@ ln -s ~/swaydots/sway/config ~/.config/sway/config
 ln -s ~/swaydots/sway/status.sh ~/.config/sway/status.sh
 ln -s ~/swaydots/sway/toggle-colors.sh ~/.config/sway/toggle-colors.sh
 
-# Theme symlinks (default to dark theme)
 ln -sf ~/swaydots/sway/theme-dark.conf ~/.config/sway/theme.conf
 ln -sf ~/swaydots/foot/foot-dark.ini ~/.config/foot/foot.ini
+ln -s ~/swaydots/micro/settings.json ~/.config/micro/settings.json
 
-ln -s ~/swaydots/aliases ~/config/aliases
+ln -s ~/swaydots/aliases ~/.config/aliases
 ln -s ~/swaydots/profile ~/.profile
 ln -s ~/swaydots/ashrc ~/.ashrc
 ln -s ~/swaydots/bashrc ~/.config/bash/bashrc
